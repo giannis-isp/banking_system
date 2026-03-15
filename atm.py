fa12fb438947e5f9cb22c2aa52857930f27cc185
giannis-isp <ioannhspatalidhs@gmail.com>
2026-03-15 23:54:51 +0200

    atm.py

--- atm.py
+++ atm.py
@@ -2,15 +2,17 @@ from Sign_up import database
 
 class ATM:
     def __init__(self, user):
-        self.balance = database[user]["balance"]
+        self.balance = database[user]["balance"] #takes the users balance from the db
         self.user = user
-        print(f"{user}, your current balance is {database[user]["balance"]} €")
+        print(f"{user}, your current balance is {database[user]["balance"]} €")#shows the balance
 
     def withdraw(self, money):
         if money > self.balance:
             print("Insufficient funds")
         else:
             self.balance -= money
+        database[self.user]["balance"] = self.balance  #updates the db   
 
     def deposit(self, money):
         self.balance += money
+        database[self.user]["balance"] = self.balance   #updates the db  
